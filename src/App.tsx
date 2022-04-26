7bcfe31860462c959c4433475e99bfc7a24b39c4
Mordi Goldstein <62645629+MGoldstein18@users.noreply.github.com>
2022-04-26 09:39:08 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThirdwebProvider, ChainId } from '@thirdweb-dev/react';
+import Nft from './components/Nft';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThirdwebProvider desiredChainId={ChainId.Rinkeby}>
+      <Nft />
+    </ThirdwebProvider>
   );
 }
 
