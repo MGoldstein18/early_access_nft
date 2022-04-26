de7049944f56566aef0a9d8cae0369130a888ddb
Mordi Goldstein <62645629+MGoldstein18@users.noreply.github.com>
2022-04-26 10:14:21 +0200

    fix(package): update outdated packages

--- scripts/createNft.ts
+++ scripts/createNft.ts
@@ -7,7 +7,7 @@ const createNFT = async () => {
         name: 'Early Access',
         description: 'This NFT will give you early access!',
         // Get the NFT from a file uploaded to IPFS
-        image: 'https://ipfs.thirdweb.com/ipfs/QmNyZpZcM73kkUnajDs6uU88V9wBe4EWxK98yaVbDfQF8K/0',
+        image: 'https://bafybeieicywyvnaher24isrxoagjxbro6qr6kbzcz2feldbquoqeag7ivm.ipfs.nftstorage.link/',
       },
     ]);
     console.log('✅ Successfully created a new NFT!');
